38a1927e39e47a6ca862b1c1a4089563e174f625
Romain Roche <romain.roche@news-republic.com>
2017-08-15 14:40:55 +0200

    chg: colors fading view

--- app/src/main/java/com/romainroche/alarms/ColorChangingView.java
+++ app/src/main/java/com/romainroche/alarms/ColorChangingView.java
@@ -14,7 +14,7 @@ public class ColorChangingView extends View {
 
     public int fromColor;
     public int toColor;
-    public long fadeTime = (long)(3.0 * 60 * 100.0); // in milliseconds
+    public long fadeTime = (long)(3.0 * 60 * 1000.0); // in milliseconds
     public long targetTime = (long)0;
 
     public ColorChangingView(Context context, AttributeSet attrs) {
@@ -37,13 +37,24 @@ public class ColorChangingView extends View {
             this.targetTime = timestamp + this.fadeTime;
         }
         long deltaT = this.targetTime - timestamp;
-        float ratio = ((float)deltaT / (float)this.fadeTime);
+        float ratio = 1 - ((float)deltaT / (float)this.fadeTime);
+
         int color0 = getResources().getInteger(this.fromColor);
+        int color0R = (color0 >> 16) & 0xff;
+        int color0G = (color0 >> 8) & 0xff;
+        int color0B = (color0) & 0xff;
+
         int color1 = getResources().getInteger(this.toColor);
-        //int color0 = (int)Long.parseLong(getResources().getString(this.fromColor));
-        //int color1 = (int)Long.parseLong(getResources().getString(this.toColor));
-        int bgColor = color0 + (int)((color1 - color0) * ratio);
-        this.setBackgroundColor(bgColor);
+        int color1R = (color1 >> 16) & 0xff;
+        int color1G = (color1 >> 8) & 0xff;
+        int color1B = (color1) & 0xff;
+
+        int resR = color0R + (int)((color1R - color0R) * ratio);
+        int resG = color0G + (int)((color1G - color0G) * ratio);
+        int resB = color0B + (int)((color1B - color0B) * ratio);
+        int res = (255 & 0xff) << 24 | (resR & 0xff) << 16 | (resG & 0xff) << 8 | (resB & 0xff);
+
+        this.setBackgroundColor(res);
         this.invalidate();
         super.draw(canvas);
     }
