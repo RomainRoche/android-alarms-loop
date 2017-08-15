14d948ce624390c1eb0f5155069657ade167fb99
Romain Roche <romain.roche@news-republic.com>
2017-08-15 14:58:02 +0200

    cosmetics

--- app/src/main/java/com/romainroche/alarms/ColorChangingView.java
+++ app/src/main/java/com/romainroche/alarms/ColorChangingView.java
@@ -1,6 +1,7 @@
 package com.romainroche.alarms;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -12,16 +13,34 @@ import android.view.View;
 
 public class ColorChangingView extends View {
 
-    public int fromColor;
-    public int toColor;
-    public long fadeTime = (long)(3.0 * 60 * 1000.0); // in milliseconds
+    private int fromColor, fromRed, fromGreen, fromBlue;
+    private int toColor, toRed, toGreen, toBlue;
+    public long fadeTime = (long)(1.0 * 10 * 1000.0); // in milliseconds
     public long targetTime = (long)0;
 
     public ColorChangingView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        this.fromColor = R.color.background0;
-        this.toColor = R.color.background1;
+        this.setFromColorResource(R.color.background0);
+        this.setToColorResource(R.color.background1);
         this.setBackgroundResource(this.fromColor);
+    }
+
+    public int getFromColorResource() { return this.fromColor; }
+    public void setFromColorResource(int resourceId) {
+        this.fromColor = resourceId;
+        int from = this.getResources().getInteger(this.fromColor);
+        this.fromRed = (from >> 16) & 0xff;
+        this.fromGreen = (from >> 8) & 0xff;
+        this.fromBlue = (from) & 0xff;
+    }
+
+    public int getToColorResource() { return this.toColor; }
+    public void setToColorResource(int resourceId) {
+        this.toColor = resourceId;
+        int to = this.getResources().getInteger(this.toColor);
+        this.toRed = (to >> 16) & 0xff;
+        this.toGreen = (to >> 8) & 0xff;
+        this.toBlue = (to) & 0xff;
     }
 
     @Override
@@ -30,8 +49,8 @@ public class ColorChangingView extends View {
         if (timestamp > this.targetTime && this.targetTime != 0) {
             this.targetTime = 0;
             int tmpColor = this.fromColor;
-            this.fromColor = toColor;
-            this.toColor = tmpColor;
+            this.setFromColorResource(this.toColor);
+            this.setToColorResource(tmpColor);
         }
         if (this.targetTime == 0) {
             this.targetTime = timestamp + this.fadeTime;
@@ -39,19 +58,9 @@ public class ColorChangingView extends View {
         long deltaT = this.targetTime - timestamp;
         float ratio = 1 - ((float)deltaT / (float)this.fadeTime);
 
-        int color0 = getResources().getInteger(this.fromColor);
-        int color0R = (color0 >> 16) & 0xff;
-        int color0G = (color0 >> 8) & 0xff;
-        int color0B = (color0) & 0xff;
-
-        int color1 = getResources().getInteger(this.toColor);
-        int color1R = (color1 >> 16) & 0xff;
-        int color1G = (color1 >> 8) & 0xff;
-        int color1B = (color1) & 0xff;
-
-        int resR = color0R + (int)((color1R - color0R) * ratio);
-        int resG = color0G + (int)((color1G - color0G) * ratio);
-        int resB = color0B + (int)((color1B - color0B) * ratio);
+        int resR = this.fromRed + (int)((this.toRed - this.fromRed) * ratio);
+        int resG = this.fromGreen + (int)((this.toGreen - this.fromGreen) * ratio);
+        int resB = this.fromBlue + (int)((this.toBlue - this.fromBlue) * ratio);
         int res = (255 & 0xff) << 24 | (resR & 0xff) << 16 | (resG & 0xff) << 8 | (resB & 0xff);
 
         this.setBackgroundColor(res);
