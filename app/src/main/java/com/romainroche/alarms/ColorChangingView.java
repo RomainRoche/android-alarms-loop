e8dd0ded1ee250c0e7f0073c82fd1f000d1c2214
Romain Roche <romain.roche@news-republic.com>
2017-08-16 09:48:03 +0200

    chg: two classes for color changing view, and count down view

--- app/src/main/java/com/romainroche/alarms/ColorChangingView.java
+++ app/src/main/java/com/romainroche/alarms/ColorChangingView.java
@@ -3,12 +3,7 @@ package com.romainroche.alarms;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 /**
  * Created by rroche on 11/08/2017.
@@ -18,18 +13,19 @@ public class ColorChangingView extends LinearLayout {
 
     private int fromColor, fromRed, fromGreen, fromBlue;
     private int toColor, toRed, toGreen, toBlue;
-    public long duration = (long)(3.0 * 60 * 1000.0); // in milliseconds
+
+    protected long timestamp, deltaT;
+
+    public long duration = (long)(1.0 * 60 * 1000.0); // in milliseconds
     public long targetTime = (long)0;
 
-    private TextView textView;
+
 
     public ColorChangingView(Context context, AttributeSet attrs) {
         super(context, attrs);
         this.setFromColorResource(R.color.background0);
         this.setToColorResource(R.color.background1);
         this.setBackgroundResource(this.fromColor);
-        View.inflate(context, R.layout.countdown, this);
-        this.textView = (TextView)findViewById(R.id.countdownTextView);
     }
 
     public int getFromColorResource() { return this.fromColor; }
@@ -50,18 +46,6 @@ public class ColorChangingView extends LinearLayout {
         this.toBlue = (to) & 0xff;
     }
 
-    private String countdownText(long deltaT) {
-        long remainingTime = deltaT;
-        long minutes = remainingTime / (long)(60 * 1000);
-        long milliseconds = remainingTime % (long)(60 * 1000);
-        long seconds = milliseconds / 1000;
-        milliseconds = milliseconds - (seconds * 1000);
-        String minutesString = minutes >= 10 ? "" : "0";
-        String secondsString = seconds >= 10 ? "" : "0";
-        String milliString = milliseconds >= 100 ? "" : milliseconds >= 10 ? "0" : "000";
-        return minutesString + minutes + ":" + secondsString + seconds + "." + milliString + milliseconds;
-    }
-
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         for(int i = 0 ; i < this.getChildCount() ; i++){
@@ -69,10 +53,9 @@ public class ColorChangingView extends LinearLayout {
         }
     }
 
-    @Override
-    public void draw(Canvas canvas) {
-        long timestamp = System.currentTimeMillis();
-        if (timestamp > this.targetTime && this.targetTime != 0) {
+    protected void getTimeData() {
+        this.timestamp = System.currentTimeMillis();
+        if (this.timestamp > this.targetTime && this.targetTime != 0) {
             this.targetTime = 0;
             int tmpColor = this.fromColor;
             this.setFromColorResource(this.toColor);
@@ -81,8 +64,13 @@ public class ColorChangingView extends LinearLayout {
         if (this.targetTime == 0) {
             this.targetTime = timestamp + this.duration;
         }
-        long deltaT = this.targetTime - timestamp;
-        float ratio = 1 - ((float)deltaT / (float)this.duration);
+        this.deltaT = this.targetTime - this.timestamp;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        this.getTimeData();
+        float ratio = 1 - ((float)this.deltaT / (float)this.duration);
 
         int resR = this.fromRed + (int)((this.toRed - this.fromRed) * ratio);
         int resG = this.fromGreen + (int)((this.toGreen - this.fromGreen) * ratio);
@@ -90,7 +78,6 @@ public class ColorChangingView extends LinearLayout {
         int res = (255 & 0xff) << 24 | (resR & 0xff) << 16 | (resG & 0xff) << 8 | (resB & 0xff);
 
         this.setBackgroundColor(res);
-        this.textView.setText(this.countdownText(deltaT));
         this.invalidate();
         super.draw(canvas);
     }
---
+++ app/src/main/java/com/romainroche/alarms/CountdownView.java
@@ -0,0 +1,43 @@
+package com.romainroche.alarms;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by rroche on 16/08/2017.
+ */
+
+public class CountdownView extends ColorChangingView {
+
+    private TextView textView;
+
+    public CountdownView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        View.inflate(context, R.layout.countdown, this);
+        this.textView = (TextView)findViewById(R.id.countdownTextView);
+    }
+
+    private String countdownText(long deltaT) {
+        long remainingTime = deltaT;
+        long minutes = remainingTime / (long)(60 * 1000);
+        long milliseconds = remainingTime % (long)(60 * 1000);
+        long seconds = milliseconds / 1000;
+        milliseconds = milliseconds - (seconds * 1000);
+        String minutesString = minutes >= 10 ? "" : "0";
+        String secondsString = seconds >= 10 ? "" : "0";
+        String milliString = milliseconds >= 100 ? "" : milliseconds >= 10 ? "0" : "000";
+        return minutesString + minutes + ":" + secondsString + seconds + "." + milliString + milliseconds;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        this.getTimeData();
+        this.textView.setText(this.countdownText(this.deltaT));
+        this.invalidate();
+        super.draw(canvas);
+    }
+
+}
